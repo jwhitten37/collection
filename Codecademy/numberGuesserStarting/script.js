f65945b123d872ad5f2659c43195d45489dde217
Justin <jut_cut_37@yahoo.com>
2022-11-10 23:03:13 +0100

    Restructured files

---
+++ Codecademy/numberGuesserStarting/script.js
@@ -0,0 +1,26 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
+ console.log(generateTarget());
+
+const compareGuesses = (humanGuess,computerGuess,targetGuess) => {
+    const humanDifference = Math.abs(targetGuess - humanGuess);
+    const computerDifference = Math.abs(targetGuess - computerGuess);
+
+    return humanDifference <= computerDifference;
+}
+
+const updateScore = winner => {
+    if (winner === "human") {
+        humanScore++;
+    } else if (winner === "computer") {
+        computerScore++;
+    }
+}
+
+const advanceRound = () => currentRoundNumber++;
---
+++ Codecademy/reactRouter/src/components/Profile.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Route, Link, Router, useRouteMatch, Redirect } from "react-router-dom";
+import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
+import EditProfileForm from "./EditProfileForm";
+
+export default function Profile () {
+  const currentUser = useSelector(selectCurrentUser)
+  const loggedIn = useSelector(selectIsLoggedIn);
+  let {url, path} = useRouteMatch();
+
+  // call useRouteMatch() to get the url and path
+  
+  // use loggedIn to return a Redirect
+  if (!loggedIn) {
+    return (
+      <Redirect to='/sign-up' />
+    )
+  }
+  return (
+    <main>
+      <h1>{currentUser.username}</h1>
+      <Link to={`${url}/edit`}>Edit</Link>
+      <Route path={`${path}/edit`}>
+        <EditProfileForm />
+      </Route>
+    </main>
+  )
+}
